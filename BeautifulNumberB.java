0e02fb455114ded8eadf3916ab89fe913a122dfc
Cesar <30559667+CesarAroon@users.noreply.github.com>
2021-04-01 01:55:29 -0500

    Add files via upload

---
+++ BeautifulNumberB.java
@@ -0,0 +1,61 @@
+
+
+import java.util.Scanner;
+
+public class BeautifulNumberB {
+
+    public static void main(String[] args) {
+        //declaracion de variables
+        int n;
+        int resultado;
+
+        // ingreso de variables
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("ingrese el valor de n: ");
+        n = entrada.nextInt();
+        //proceso
+        resultado = minimumNumber(n);
+
+        //resultado
+        System.out.println("minimo numero: " + resultado);
+
+    }
+
+    public static int minimumNumber(int n) {
+        int resultado;
+        if (n < 1000 || n > 9000) {
+            return 0;
+        } else {
+
+            if (isAllDifferentNumbers(n) == true) {
+                resultado = n;
+            } else {
+                resultado = minimumNumber(n + 1);
+            }
+            //salida
+            return resultado;
+
+        }
+    }
+
+    private static boolean isAllDifferentNumbers(int n) {
+        boolean tieneNumdif;
+        int contadorCoincidencias = 0;
+        String numeroCadena = Integer.toString(n);
+        for (int i = 0; i < numeroCadena.length() - 1; i++) {
+            for (int j = 1; j < numeroCadena.length() - i; j++) {
+                if (numeroCadena.charAt(i) == numeroCadena.charAt(j + i)) {
+                    contadorCoincidencias = contadorCoincidencias + 1;
+                }
+
+            }
+        }
+        if (contadorCoincidencias > 0) {
+            tieneNumdif = false;
+        } else {
+            tieneNumdif = true;
+        }
+        return tieneNumdif;
+    }
+
+}
